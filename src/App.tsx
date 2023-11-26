d7a8c97e1623acf324c6c2c3c719b9248dea0f42
FacuReartes <facureartes9@gmail.com>
2023-11-26 16:26:31 -0300

    ultima cosa

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import LogIn from "./components/LogIn";
 import Home from "./components/Home";
 import SignUp from "./components/SignUp";
@@ -14,6 +14,7 @@ function App() {
           <Route path="/home" Component={Home}/>
           <Route path="/login" Component={LogIn}/>
           <Route path="/signup" Component={SignUp}/>
+          <Route path="*" element={<Navigate to="/home" replace />}/>
         </Routes>
       </Router>
     </UserContextProvider>
